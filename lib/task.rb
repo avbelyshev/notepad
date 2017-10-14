0c161c393564419d2826095952b410d2668b8ef1
avb <avbelishev@gmail.com>
2017-10-15 00:28:51 +0300

    New version, with SQLite support

--- lib/task.rb
+++ lib/task.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
-
+#
+# Подключим встроенный в руби класс Date для работы с датами
 require 'date'
 
+# Класс «Задача», разновидность базового класса «Запись»
 class Task < Post
   def initialize
     super
@@ -25,5 +27,24 @@ class Task < Post
     time_string = "Создано: #{@created_at.strftime('%Y.%m.%d, %H:%M:%S')} \n\r"
 
     [deadline, @text, time_string]
+  end
+
+  # Метод to_db_hash у Задачи добавляет два ключа в хэш
+  def to_db_hash
+    # Вызываем родительский метод to_db_hash ключевым словом super. К хэшу,
+    # который он вернул добавляем специфичные для этого класса поля методом
+    # Hash#merge
+    super.merge('text' => @text, 'due_date' => @due_date.to_s)
+  end
+
+  # Метод load_data у Задачи считывает дополнительно due_date задачи
+  def load_data(data_hash)
+    # Сперва дергаем родительский метод load_data для общих полей. Обратите
+    # внимание, что вызов без параметров тут эквивалентен super(data_hash), так
+    # как те же параметры будут переданы методы super автоматически.
+    super
+
+    # Теперь достаем из хэша специфичное только для задачи значение due_date
+    @due_date = Date.parse(data_hash['due_date'])
   end
 end
